2d5ab2056e9a9fa9e828e4c865dd20519598e83a
jiechic <jiechic@xiaoman.cn>
2024-06-24 09:22:34 +0800

    feat: Build goRouter using Goute instead of goRouterBuilder was success

--- lib/src/module/login/login_route.dart
+++ lib/src/module/login/login_route.dart
@@ -1,54 +1,27 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-part 'login_route.g.dart';
-
-@TypedGoRoute<LoginRoute>(
-  path: '/login',
-  routes: [
-    TypedGoRoute<LoginPasswordRoute>(
-      path: "password",
-    ),
-  ],
-)
-class LoginRoute extends GoRouteData {
-  String? returnUrl;
-
-  LoginRoute({
-    this.returnUrl,
-  });
-
-
-  @override
-  FutureOr<String?> redirect(BuildContext context, GoRouterState state) {
-    if (Uri.parse(location).path == state.uri.path) {
-      return Uri(
-              path: "/login/password",
-              queryParameters: state.uri.queryParameters)
-          .toString();
-    }
-    return null;
-  }
-}
-
-class LoginPasswordRoute extends GoRouteData {
-  String? returnUrl;
-
-  LoginPasswordRoute({
-    this.returnUrl,
-  });
-
-  @override
-  Widget build(BuildContext context, GoRouterState state) {
-    return TextButton(
-      onPressed: () {
-
-      },
-      child: const Text("back on login-password"),
-    );
-  }
-}
-
-
+final loginRoute = GoRoute(
+    path: "/login",
+    redirect: (context, state) {
+      if ("/login" == state.uri.path) {
+        return Uri(
+                path: "/login/password",
+                queryParameters: state.uri.queryParameters)
+            .toString();
+      }
+      return null;
+    },
+    routes: [
+      GoRoute(
+          path: "password",
+          builder: (
+            context,
+            state,
+          ) {
+            return TextButton(
+              onPressed: () {},
+              child: const Text("back on login-password"),
+            );
+          })
+    ]);
--- lib/src/router.dart
+++ lib/src/router.dart
@@ -1,18 +1,18 @@
 import 'package:go_router/go_router.dart';
 import 'package:go_router_demo/src/module/home/home_route.dart';
 
-import 'module/login/login_route.dart';
+import 'package:go_router_demo/src/module/login/login_route.dart';
 
 var isLogin = false;
 
 final goRouter = GoRouter(
   routes: [
     homeRoute,
-    $loginRoute,
+    loginRoute,
   ],
   redirect: (context, state) async {
     ///is login path ,do not redirect
-    if (state.matchedLocation.contains(($loginRoute as GoRoute).path) == true) {
+    if (state.matchedLocation.contains((loginRoute).path) == true) {
       return null;
     }
     if (!isLogin) {
